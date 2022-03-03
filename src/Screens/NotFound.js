4bb8caf90d1816eaa2eb1fdaa0de5277284a941f
AbdullahAkb <abdullahakb111@gmail.com>
2022-03-03 15:45:47 +0500

    master

---
+++ src/Screens/NotFound.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Icon1 from 'react-native-vector-icons/Ionicons'
+import { useNavigation } from '@react-navigation/native'
+
+
+
+export default function NotFound() {
+    return (
+        <View>
+            <Icon1 name='arrow-back' size={25} color={'#05CE91'}
+            onPress={()=>{}}
+            />
+        </View>
+    )
+}
